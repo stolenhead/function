e5aca972694912bb55529e30a835e7ff1d7d02c6
ANDREA <andrea.palomino.ruby@gmail.com>
2016-09-01 16:29:01 -0700

    function

---
+++ consec/consec.js
@@ -0,0 +1,6 @@
+function sucesion(n){
+	for(var i=1 ;i<=n;i++){
+		document.write(i);
+	}
+}
+sucesion(4);
---
+++ consec2/consec2.js
@@ -0,0 +1,6 @@
+function sucesion(n){
+	for(var i=n ;i>=1;i--){
+		document.write(i);
+	}
+}
+sucesion(4);
